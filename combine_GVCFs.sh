dcc73f7c21cfcc37c0c8abb9cae62f0819bd8d66
Peter <peter.innes@colorado.edu>
2019-07-19 16:53:43 -0600

    working on adding commandline args

--- combine_GVCFs.sh
+++ combine_GVCFs.sh
@@ -7,15 +7,26 @@
 #SBATCH --output=/scratch/Users/pein7187/slurm_out_err/tG_combine_GVCFs_all_%a.out
 #SBATCH --error=/scratch/Users/pein7187/slurm_out_err/tG_combine_GVCFs_all_%a.err
 
-ref=/scratch/Users/pein7187/taeGut/Taeniopygia_guttata.taeGut3.2.4.dna.toplevel.fa
-vcf_csv=/scratch/Users/pein7187/vcf/zebra_finch/joint_calling/all_spp_gvcf_list_tG.csv
-outputdir=/scratch/Users/pein7187/vcf/zebra_finch/joint_calling/
-output_vcf_prefix=all_chickadee_spp_tG
+#old variables
+#ref=/scratch/Users/pein7187/taeGut/Taeniopygia_guttata.taeGut3.2.4.dna.toplevel.fa
+#vcf_csv=/scratch/Users/pein7187/vcf/zebra_finch/joint_calling/all_spp_gvcf_list_tG.csv
+#outputdir=/scratch/Users/pein7187/vcf/zebra_finch/joint_calling/
+#output_vcf_prefix=all_chickadee_spp_tG
 
 module load gatk
 module load samtools
 
 set -exuo pipefail
+
+if [ $# -lt 1 ]
+then
+    echo "
+    Combines and genotypes GVCFs, then filters the results VCF table, and removes indels.
+    Input is all GVCF files generated from HaplotypeCaller.
+    Output is out final, filtered VCF table which will be used for downstream a
+
+    #insert arguments here
+    "
 
 #echo "STARTING reference=$ref vcf_csv=$vcf_csv output_vcf_prefix=$output_vcf_prefix $0"
 
@@ -31,9 +42,17 @@ for vcf in $( cat $vcf_csv | tr ',' '\n' ); do
 
 done
 
+#combine gvcfs
+
 gatk CombineGVCFs -R $ref -O $outputdir"$output_vcf_prefix".joint.vcf $vcf_args
 
+
+#genotype gvcfs
+
 gatk GenotypeGVCFs -R $ref -O $outputdir"$output_vcf_prefix".vcf -V $outputdir"$output_vcf_prefix".joint.vcf
+
+
+#filter
 
 gatk VariantFiltration \
 -R $ref \
@@ -48,3 +67,12 @@ gatk VariantFiltration \
 
 rm $outputdir"$output_vcf_prefix".joint.vcf
 rm $outputdir"$output_vcf_prefix".vcf
+
+#remove indels
+gatk SelectVariants \
+-R $ref \
+--variant $outputdir"$output_vcf_prefix"_gatkfiltered.vcf \
+-o $outputdir"$output_vcf_prefix"_gatkfiltered_no_indels.vcf \
+--selectTypeToExclude INDEL
+
+rm $outputdir"$output_vcf_prefix"_gatkfiltered.vcf
