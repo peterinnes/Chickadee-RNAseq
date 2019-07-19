6f6b2df6e225194d2b32b9e1a021b97b909abc4e
Peter <peter.innes@colorado.edu>
2019-07-19 15:50:56 -0600

    cleaning up repo, deleted old test scripts

--- tests/STAR_mapping_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load STAR
-
-rundir=/scratch/Users/pein7187/STAR_mapping_test1pass
-mkdir $rundir
-cd $rundir
-
-STAR --genomeDir /scratch/Users/pein7187/taeGut --readFilesCommand gunzip -c --readFilesIn /scratch/Users/pein7187/chickadee_fastq/BCCH/Chickadee_CU54780_CGATGT_L003_R1_001.fastq.gz --runThreadN 4
